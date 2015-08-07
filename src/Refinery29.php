f70f40b68e7bf0aa214c5ba9d92ab4e270013e37
Andreas Möller <am@localheinz.com>
2015-08-08 00:19:17 +0200

    Enhancement: Assert a few default configuration values

--- src/Refinery29.php
+++ src/Refinery29.php
@@ -2,6 +2,33 @@
 
 namespace Refinery29\CS\Config;
 
-class Refinery29
+use Symfony\CS\Config\Config;
+use Symfony\CS\FixerInterface;
+
+class Refinery29 extends Config
 {
+    public function getName()
+    {
+        return 'refinery29';
+    }
+
+    public function getDescription()
+    {
+        return 'The configuration for Refinery29 PHP applications';
+    }
+
+    public function getLevel()
+    {
+        return FixerInterface::PSR2_LEVEL;
+    }
+
+    public function usingCache()
+    {
+        return true;
+    }
+
+    public function usingLinter()
+    {
+        return true;
+    }
 }
--- tests/Refinery29Test.php
+++ tests/Refinery29Test.php
@@ -3,11 +3,26 @@
 namespace Refinery29\CS\Config\Test;
 
 use Refinery29\CS\Config\Refinery29;
+use Symfony\CS\ConfigInterface;
+use Symfony\CS\FixerInterface;
 
 class Refinery29Test extends \PHPUnit_Framework_TestCase
 {
-    public function testClassExists()
+    public function testImplementsInterface()
     {
-        $this->assertTrue(class_exists(Refinery29::class));
+        $config = new Refinery29();
+
+        $this->assertInstanceOf(ConfigInterface::class, $config);
+    }
+
+    public function testValues()
+    {
+        $config = new Refinery29();
+
+        $this->assertSame('refinery29', $config->getName());
+        $this->assertSame('The configuration for Refinery29 PHP applications', $config->getDescription());
+        $this->assertSame(FixerInterface::PSR2_LEVEL, $config->getLevel());
+        $this->assertTrue($config->usingCache());
+        $this->assertTrue($config->usingLinter());
     }
 }
