9adbbe888f9517373b57d07577019be61170d0f5
Andreas Möller <am@localheinz.com>
2015-08-08 00:41:49 +0200

    Enhancement: Assert config has desired Symfony fixers enabled

--- src/Refinery29.php
+++ src/Refinery29.php
@@ -7,14 +7,11 @@ use Symfony\CS\FixerInterface;
 
 class Refinery29 extends Config
 {
-    public function getName()
+    public function __construct($name = 'refinery29', $description = 'The configuration for Refinery29 PHP applications')
     {
-        return 'refinery29';
-    }
+        parent::__construct($name, $description);
 
-    public function getDescription()
-    {
-        return 'The configuration for Refinery29 PHP applications';
+        $this->fixers = $this->getEnabledSymfonyFixers();
     }
 
     public function getLevel()
@@ -30,5 +27,55 @@ class Refinery29 extends Config
     public function usingLinter()
     {
         return true;
+    }
+
+    /**
+     * @return array
+     */
+    private function getEnabledSymfonyFixers()
+    {
+        return [
+            'alias_functions',
+            'blankline_after_open_tag',
+            'double_arrow_multiline_whitespaces',
+            'duplicate_semicolon',
+            'empty_return',
+            'extra_empty_lines',
+            'include',
+            'join_function',
+            'list_commas',
+            'multiline_array_trailing_comma',
+            'namespace_no_leading_whitespace',
+            'new_with_braces',
+            'no_blank_lines_after_class_opening',
+            'no_empty_lines_after_phpdocs',
+            'object_operator',
+            'operators_spaces',
+            'phpdoc_align',
+            'phpdoc_indent',
+            'phpdoc_inline_tag',
+            'phpdoc_no_access',
+            'phpdoc_no_empty_return',
+            'phpdoc_no_package',
+            'phpdoc_scalar',
+            'phpdoc_separation',
+            'phpdoc_to_comment',
+            'phpdoc_trim',
+            'phpdoc_type_to_var',
+            'phpdoc_var_without_name',
+            'remove_leading_slash_use',
+            'remove_lines_between_uses',
+            'return',
+            'single_array_no_trailing_comma',
+            'single_blank_line_before_namespace',
+            'single_quote',
+            'spaces_before_semicolon',
+            'spaces_cast',
+            'standardize_not_equal',
+            'ternary_spaces',
+            'trim_array_spaces',
+            'unalign_double_arrow',
+            'whitespacy_lines',
+        ];
     }
 }
--- tests/Refinery29Test.php
+++ tests/Refinery29Test.php
@@ -25,4 +25,66 @@ class Refinery29Test extends \PHPUnit_Framework_TestCase
         $this->assertTrue($config->usingCache());
         $this->assertTrue($config->usingLinter());
     }
+
+    public function testHasEnabledSymfonyFixers()
+    {
+        $config = new Refinery29();
+
+        $missingFixers = array_diff($this->getEnabledSymfonyFixers(), $config->getFixers());
+
+        $this->assertCount(0, $missingFixers, sprintf(
+            'Symfony fixer(s) "%s" should be enabled',
+            implode('", "', $missingFixers)
+        ));
+    }
+
+    /**
+     * @return array
+     */
+    private function getEnabledSymfonyFixers()
+    {
+        return [
+            'alias_functions',
+            'blankline_after_open_tag',
+            'double_arrow_multiline_whitespaces',
+            'duplicate_semicolon',
+            'empty_return',
+            'extra_empty_lines',
+            'include',
+            'join_function',
+            'list_commas',
+            'multiline_array_trailing_comma',
+            'namespace_no_leading_whitespace',
+            'new_with_braces',
+            'no_blank_lines_after_class_opening',
+            'no_empty_lines_after_phpdocs',
+            'object_operator',
+            'operators_spaces',
+            'phpdoc_align',
+            'phpdoc_indent',
+            'phpdoc_inline_tag',
+            'phpdoc_no_access',
+            'phpdoc_no_empty_return',
+            'phpdoc_no_package',
+            'phpdoc_scalar',
+            'phpdoc_separation',
+            'phpdoc_to_comment',
+            'phpdoc_trim',
+            'phpdoc_type_to_var',
+            'phpdoc_var_without_name',
+            'remove_leading_slash_use',
+            'remove_lines_between_uses',
+            'return',
+            'single_array_no_trailing_comma',
+            'single_blank_line_before_namespace',
+            'single_quote',
+            'spaces_before_semicolon',
+            'spaces_cast',
+            'standardize_not_equal',
+            'ternary_spaces',
+            'trim_array_spaces',
+            'unalign_double_arrow',
+            'whitespacy_lines',
+        ];
+    }
 }
