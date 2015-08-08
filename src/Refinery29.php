30d0b19b593fd1531de43a7c73417145392e329a
Andreas Möller <am@localheinz.com>
2015-08-08 22:49:14 +0200

    Enhancement: Enable unused_use fixer

--- src/Refinery29.php
+++ src/Refinery29.php
@@ -78,6 +78,7 @@ class Refinery29 extends Config
             'ternary_spaces',
             'trim_array_spaces',
             'unalign_double_arrow',
+            'unused_use',
             'whitespacy_lines',
         ];
     }
