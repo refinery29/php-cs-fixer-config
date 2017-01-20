f5a3b9a7b7d5f69b407fc276db1e45371a804ed5
Andreas Möller <am@localheinz.com>
2017-01-20 13:04:39 +0100

    Enhancement: Configure visibility_required fixer

--- src/Refinery29.php
+++ src/Refinery29.php
@@ -162,6 +162,11 @@ final class Refinery29 extends Config
             'trailing_comma_in_multiline_array' => true,
             'trim_array_spaces' => true,
             'unary_operator_spaces' => true,
+            'visibility_required' => [
+                'const',
+                'property',
+                'method',
+            ],
             'whitespace_after_comma_in_array' => true,
         ];
 
