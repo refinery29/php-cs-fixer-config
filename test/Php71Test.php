7298dc6b2489b60bfbbf49e209d78e76fc25ad74
Andreas Möller <am@localheinz.com>
2017-04-06 17:29:06 +0200

    Enhancement: Enable non_printable_character fixer

--- test/Php71Test.php
+++ test/Php71Test.php
@@ -106,7 +106,7 @@ final class Php71Test extends AbstractConfigTestCase
             'no_useless_return' => true,
             'no_whitespace_before_comma_in_array' => true,
             'no_whitespace_in_blank_line' => true,
-            'non_printable_character' => false, // have not decided to use this one (yet)
+            'non_printable_character' => true,
             'normalize_index_brace' => true,
             'not_operator_with_space' => false, // have decided not to use it
             'not_operator_with_successor_space' => false, // have decided not to use it
